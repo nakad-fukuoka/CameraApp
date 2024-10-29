881bf95a15c4971b691c1a47c2c55e5a757b9079
nakachi <daiki.nakachi.dn@nnr-g.com>
2024-10-29 23:27:32 +0900

    commit test

--- Cameraapp/App.xaml.cs
+++ Cameraapp/App.xaml.cs
@@ -7,8 +7,19 @@ namespace Cameraapp
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var w = new MainForm();
+            w.Show();
+        }
 
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+
+    }
 }
